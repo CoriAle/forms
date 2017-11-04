c4501238c0cc6a211381e4df5d2dd22d75a790b3
Laptop Nimatuj <1995coral@hotmail.es>
2017-11-03 23:41:43 -0600

    Primer

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [
+		`
+		.ng-invalid.ng-touched:not(form){
+			border: 1px solid red;
+		}
+		`
+  ]
+})
+export class TemplateComponent  {
+
+	usuario:Object={
+		nombre: null,
+		apellido: null,
+		correo: null,
+		pais: "",
+		sexo: "Mujer",
+		acepta: true
+	}
+	paises = [{
+		codigo: "GT",
+		nombre: "Guatemala"
+	},
+	{
+		codigo: "ESP",
+		nombre: "España"
+	}
+	]
+
+	sexos = ["Hombre", "Mujer", "Sin definir"]
+  constructor() { }
+
+
+  guardar(forma:NgForm){
+  	console.log("Formulario posteado")
+  	console.log(forma);
+  }
+
+}
